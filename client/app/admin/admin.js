90e967272bea95bb6c42d31c9ae721b16bd8c972
amrini  abdelilah <amrini.abdelilah@gmail.com>
2015-10-20 22:24:16 +0200

    upload the app

---
+++ client/app/admin/admin.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('webSiteAppMeskiApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('admin', {
+        url: '/admin',
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://DevMeski:DevMeski123@ds041494.mongolab.com:41494/websitemeskiapp'
+  },
+
+  seedDB: true
+};
